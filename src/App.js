e272301d481398aa8a9a7b0e896ff15672ac7f70
Carol Chung <cch5ng@gmail.com>
2020-10-29 21:54:05 -0700

    2 state summary (#11)
    * cleanup
    
    * stub out the app data storage and outline of the different event handlers
    
    * stub out logic for training session init
    
    * test basic functionality for next/done buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import {getRandomIndexes} from './util';
 import Data from './Apprentice_TandemFor400_Data.json';
@@ -6,27 +6,84 @@ import Data from './Apprentice_TandemFor400_Data.json';
 function App() {
   let totalQuestionsCount = Data.length;
 
-  //tests
-  console.log('getRandomIndexes(10, 10)', getRandomIndexes(10, 10));
+  const [currentQuizQuestionIndexes, setCurrentQuizQuestionIndexes] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuizAnswers, setCurrentQuizAnswers] = useState({}); // key should be question name
+  const [currentQuizScore, setCurrentQuizScore] = useState(null);
+  const [appState, setAppState] = useState(0); //0 - not started; 1 - quiz in session; 2 - quiz was completed
 
-  console.log('getRandomIndexes(totalQuestionsCount, 10)', getRandomIndexes(totalQuestionsCount, 10));
+  const handleStartQuiz = () => {
+    initQuiz();
+    setAppState(1);
+  }
+
+  const handleAnswerSubmit = () => {
+    //store the answer
+    //evaluate if answer is correct
+    //increment curQuestionIndex
+  }  
+
+  const handleNextBtnClick = () => {
+    if (currentQuestionIndex < 9) {
+      //handle next button
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      //handle done btn click
+      //trigger scoring
+      setAppState(2);
+    }
+  }
+
+  const initQuiz = () => {
+    console.log('TODO INIT QUIZ')
+    //console.log('getRandomIndexes(totalQuestionsCount, 10)', getRandomIndexes(totalQuestionsCount, 10));
+    let currentQuizRandomQuestionIndexes = getRandomIndexes(totalQuestionsCount, 10);
+    console.log('currentQuizRandomQuestionIndexes', currentQuizRandomQuestionIndexes);
+    setCurrentQuizQuestionIndexes(currentQuizRandomQuestionIndexes);
+  }
+
+  const scoreQuiz = () => {
+    
+  }
+
+  //tests
+  //console.log('getRandomIndexes(10, 10)', getRandomIndexes(10, 10));
+  let curDataIndex = currentQuizQuestionIndexes[currentQuestionIndex];
+  let curQuestion = Data[curDataIndex];
+  let buttonLabel = currentQuestionIndex < 9 ? 'Next': 'Done';
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        TRIVIA BOOTCAMP
       </header>
+      <main>
+        {/* different states
+          1 quiz has not started yet
+          2 quiz is in session
+          3 quiz has ended
+        */}
+        {appState === 0 && (
+          <button onClick={handleStartQuiz}>Start Training</button>
+        )}
+
+        {appState === 1 && (
+          <React.Fragment>
+            <div>Training in session</div>
+            <div>
+              <h1>Question</h1>
+              <div>{curQuestion.question}</div>
+            </div>
+            <div><button onClick={handleNextBtnClick}>{buttonLabel}</button></div>
+          </React.Fragment>
+        )}
+
+        {appState === 2 && (
+          <div>Training is done</div>
+        )}
+  
+      </main>
     </div>
   );
 }
